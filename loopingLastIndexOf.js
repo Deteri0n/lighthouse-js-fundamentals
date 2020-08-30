3a34bbd659eab81f872f13b028f9d0a0e2271f2f
Etienne LC <etlecoent@gmail.com>
2020-08-30 20:02:18 +0000

    Looping - lastIndexOf challenge

---
+++ loopingLastIndexOf.js
@@ -0,0 +1,15 @@
+function lastIndexOf(array, number){
+  let index = -1;
+  array.forEach(function(value,indexTemp){
+    if (value === number){
+      index = indexTemp;
+    }
+  });
+  return index;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
